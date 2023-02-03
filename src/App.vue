b99f6d13b7c83e59b54e46e4232f98a3b7f9e886
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-02-02 19:33:34 -0500

    header and base card components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-	<ul>
-		<stored-resources :resources="storedResources"></stored-resources>
-	</ul>
+<the-header title="Learning Resources"></the-header>
+	<stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
 import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
 	components: {
 		StoredResources,
+		TheHeader,
 	},
 	data() {
 		return {
