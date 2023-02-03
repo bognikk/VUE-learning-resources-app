e4714d6c1ebd6dcf0423310bb395fb0f6f2c7f32
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-02-03 12:42:53 -0500

    add resource and the resources components

--- src/components/learning-resources/StoredResources.vue
+++ src/components/learning-resources/StoredResources.vue
@@ -14,19 +14,19 @@
 import LearningResource from './LearningResource.vue';
 
 export default {
+	inject: ['resources'],
 	components: {
 		LearningResource,
 	},
-	props: ['resources'],
 };
 </script>
 
 <style scoped>
 ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  margin: auto;
-  max-width: 40rem;
+	list-style: none;
+	margin: 0;
+	padding: 0;
+	margin: auto;
+	max-width: 40rem;
 }
 </style>
